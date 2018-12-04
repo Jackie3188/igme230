7e80d51eb09d0889902bfb39fa82899aa77b5687
Jackie Lando <jackielando@Jackies-MacBook-Pro-2.local>
2018-12-04 16:31:43 -0500

    imagechanger

--- portfolio2/main.js
+++ portfolio2/main.js
@@ -4,10 +4,11 @@
 // profile page - click event
 
 // select image from HTML doc
-var myImage = document.querySelector('img.peeky');
+var myImage = document.querySelector("#peeky");
 
 //create function with if statement to change src image when clicked
 myImage.onclick = function(){
+    console.log("I'm clicked!");
     //mySrc will be equal to source string for img
     var mySrc = myImage.getAttribute('src');
     //if the src string is = to image 1 when clicked
