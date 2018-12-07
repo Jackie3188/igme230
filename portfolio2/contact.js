654453dbc54eec849869e8850fd64a6dd4b2e6a0
Jackie Lando <jackielando@Jackies-MacBook-Pro-2.local>
2018-12-07 00:11:36 -0500

    portfolio2 update

---
+++ portfolio2/contact.js
@@ -0,0 +1,22 @@
+// Contact JS
+
+// I M A G E   C H A N G E R
+// profile page - click event
+
+// select image from HTML doc
+var myImage = document.querySelector("#peeky");
+
+//create function with if statement to change src image when clicked
+myImage.onclick = function(){
+    console.log("I'm clicked!");
+    //mySrc will be equal to source string for img
+    var mySrc = myImage.getAttribute('src');
+    //if the src string is = to image 1 when clicked
+    if(mySrc === 'images/peek1.png'){
+        //change to image 2
+        myImage.setAttribute('src', 'images/peek2.png');
+    } else {
+        //otherwise remain image 1
+        myImage.setAttribute('src', 'images/peek1.png');
+    }
+}
