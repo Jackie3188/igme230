1c9d8ba4eeaf8d044cc8371dc74ad98bdaf294d2
Jackie Lando <jackielando@Jackies-MacBook-Pro-2.local>
2018-12-09 13:30:57 -0500

    final revisions P2

--- portfolio2/contact.js
+++ portfolio2/contact.js
@@ -4,7 +4,7 @@
 // profile page - click event
 
 // select image from HTML doc
-var myImage = document.querySelector("#peeky");
+var myImage = document.querySelector("#phonemail");
 
 //create function with if statement to change src image when clicked
 myImage.onclick = function(){
@@ -12,11 +12,11 @@ myImage.onclick = function(){
     //mySrc will be equal to source string for img
     var mySrc = myImage.getAttribute('src');
     //if the src string is = to image 1 when clicked
-    if(mySrc === 'images/peek1.png'){
+    if(mySrc === 'images/contact1.png'){
         //change to image 2
-        myImage.setAttribute('src', 'images/peek2.png');
+        myImage.setAttribute('src', 'images/contact2.png');
     } else {
         //otherwise remain image 1
-        myImage.setAttribute('src', 'images/peek1.png');
+        myImage.setAttribute('src', 'images/contact1.png');
     }
 }
